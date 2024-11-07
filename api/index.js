4f80a1b838b4516f98453142e2e8dbda906081a9
Izzat <izzatmuz@gmail.com>
2024-11-07 14:27:26 +0800

    updated index.js and added .env

--- api/index.js
+++ api/index.js
@@ -30,59 +30,41 @@ async function getPostgresVersion() {
 
 getPostgresVersion();
 
-// Delete review by id endpoint
-app.delete("/reviews/review/:id", async (req, res) => {
+// Delete booking by id endpoint
+app.delete("/bookings/:id", async (req, res) => {
     const id = req.params.id;
     const client = await pool.connect();
     try {
-        const deleteQuery = "DELETE FROM reviews WHERE id = $1";
+        const deleteQuery = "DELETE FROM bookings WHERE id = $1";
         await client.query(deleteQuery, [id]);
-        res.json({ status: "success", message: "Review deleted successfully" });
+        res.json({ status: "success", message: "Booking deleted successfully" });
     } catch (error) {
-        console.error("error", error.message);
+        console.error("Error: ", error.message);
         res.status(500).json({ error: error.message });
     } finally {
         client.release();
     }
 });
 
-// Delete product by id endpoint
-app.delete("/products/product/:id", async (req, res) => {
-    const id = req.params.id;
-    const client = await pool.connect();
-    try {
-        const deleteQuery = "DELETE FROM products WHERE id = $1";
-        await client.query(deleteQuery, [id]);
-        res.json({ status: "success", message: "Product deleted successfully" });
-    } catch (error) {
-        console.error("error", error.message);
-        res.status(500).json({ error: error.message });
-    } finally {
-        client.release();
-    }
-});
-
-// Update product by id endpoint
-app.put("/products/product/:id", async (req, res) => {
+// Update booking by id endpoint
+app.put("/bookings/:id", async (req, res) => {
     const id = req.params.id;
     const updatedData = req.body;
     const client = await pool.connect();
     try {
         const updateData =
-            "UPDATE products SET name=$1, image_url=$2, specification=$3, pros=$4, cons=$5, referral_link=$6, video_url=$7, tagline=$8 WHERE id=$9";
+            "UPDATE bookings SET title=$1, description=$2, date=$3, time=$4, phone_number=$5, email=$6 WHERE id=$7";
         const queryData = [
-            updatedData.name,
-            updatedData.image_url,
-            updatedData.specification,
-            updatedData.pros,
-            updatedData.cons,
-            updatedData.referral_link,
-            updatedData.video_url,
-            updatedData.tagline,
+            updatedData.title,
+            updatedData.description,
+            updatedData.date,
+            updatedData.time,
+            updatedData.phone_number,
+            updatedData.email,
             id,
         ];
         await client.query(updateData, queryData);
-        res.json({ status: "success", message: "Product updated successfully" });
+        res.json({ status: "success", message: "Booking updated successfully" });
     } catch (error) {
         console.error("error", error.message);
         res.status(500).json({ error: error.message });
@@ -91,139 +73,130 @@ app.put("/products/product/:id", async (req, res) => {
     }
 });
 
-// Show reviews by product id endpoint
-app.get("/reviews/product/:id", async (req, res) => {
-    const { id } = req.params;
+// Show booking by user endpoint
+app.get("/bookings/user/:user_id", async (req, res) => {
+    const { user_id } = req.params;
     const client = await pool.connect();
 
+    try {
+        const posts = await client.query(
+            "SELECT * FROM bookings WHERE user_id = $1",
+            [user_id],
+        );
+        if (posts.rowCount > 0) {
+            res.json(posts.rows);
+        } else {
+            res.status(404).json({ error: "no bookings found for this user" });
+        }
+    } catch (error) {
+        console.error("Error: ", error.message);
+        res.status(500).json({ error: error.message });
+    } finally {
+        client.release();
+    }
+});
+
+// Create booking endpoint
+app.post("/bookings", async (req, res) => {
+    const client = await pool.connect();
+    try {
+        const data = {
+            title: req.body.title,
+            description: req.body.description,
+            date: req.body.date,
+            time: req.body.time,
+            phone_number: req.body.phone_number,
+            email: req.body.email,
+            user_id: req.body.user_id,
+            created_at: new Date().toISOString(),
+        };
+        const query =
+            "INSERT INTO bookings (title, description, date, time, phone_number, email, user_id, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id";
+        const params = [
+            data.title,
+            data.description,
+            data.date,
+            data.time,
+            data.phone_number,
+            data.email,
+            data.user_id,
+            data.created_at,
+        ];
+        const result = await client.query(query, params);
+        data.id = result.rows[0].id;
+        res.json({
+            status: "success",
+            data: data,
+            message: "Booking created successfully",
+        });
+    } catch (error) {
+        console.error("error", error.message);
+    } finally {
+        client.release();
+    }
+});
+
+// Signup endpoint
+app.post("/signup", async (req, res) => {
+    const client = await pool.connect();
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const userResult = await client.query(
+            "SELECT * FROM users WHERE username = $1",
+            [username],
+        );
+
+        if (userResult.rows.length > 0) {
+            return res.status(400).json({ message: "Username already taken" });
+        }
+
+        await client.query(
+            "INSERT INTO users (username, password) VALUES ($1, $2)",
+            [username, hashedPassword],
+        );
+
+        res.status(201).json({ message: "User registered succesfully" });
+    } catch (error) {
+        console.error("Error: ", error.message);
+        res.status(500).json({ error: error.message });
+    } finally {
+        client.release();
+    }
+});
+
+// Login endpoint
+app.post("/login", async (req, res) => {
+    const client = await pool.connect();
     try {
         const result = await client.query(
-            "SELECT * FROM reviews WHERE product_id = $1",
-            [id],
+            "SELECT * FROM users WHERE username = $1",
+            [req.body.username],
         );
-        if (result.rowCount > 0) {
-            res.json(result.rows);
-        } else {
-            res.status(404).json({ error: "Reviews not found" });
-        }
-    } catch (error) {
-        console.error("error", error.message);
-        res.status(500).json({ error: error.message });
-    } finally {
-        client.release();
-    }
-});
 
-// Create review endpoint
-app.post("/reviews", async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const data = {
-            review: req.body.review,
-            recommend: req.body.recommend,
-            product_id: req.body.product_id,
-            created_by: req.body.created_by,
-            created_at: new Date().toISOString(),
-        };
-        const query =
-            "INSERT INTO reviews (review, recommend, product_id, created_by, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING id";
-        const params = [
-            data.review,
-            data.recommend,
-            data.product_id,
-            data.created_by,
-            data.created_at,
-        ];
-        const result = await client.query(query, params);
-        data.id = result.rows[0].id;
-        res.json({
-            status: "success",
-            data: data,
-            message: "Review created successfully",
+        const user = result.rows[0];
+
+        if (!user)
+            return res
+                .status(400)
+                .json({ message: "Username or password incorrect" });
+
+        const passwordIsValid = await bcrypt.compare(
+            req.body.password,
+            user.password,
+        );
+        if (!passwordIsValid)
+            return res.status(400).json({ auth: false, token: null });
+
+        var token = jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, {
+            expiresIn: 86400,
         });
-    } catch (error) {
-        console.error("error", error.message);
-    } finally {
-        client.release();
-    }
-});
 
-// Show product by id endpoint
-app.get("/products/product/:id", async (req, res) => {
-    const { id } = req.params;
-    const client = await pool.connect();
-
-    try {
-        const result = await client.query("SELECT * FROM products WHERE id = $1", [
-            id,
-        ]);
-        if (result.rowCount > 0) {
-            res.json(result.rows);
-        } else {
-            res.status(404).json({ error: "Product not found" });
-        }
+        res.status(200).json({ auth: true, token: token });
     } catch (error) {
-        console.error("error", error.message);
+        console.error("Error: ", error.message);
         res.status(500).json({ error: error.message });
-    } finally {
-        client.release();
-    }
-});
-
-// Show all products endpoint
-app.get("/products", async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const query = "SELECT * FROM products";
-        const result = await client.query(query);
-        res.json(result.rows);
-    } catch (error) {
-        console.error("error", error.message);
-        res.status(500).json({ error: error.message });
-    } finally {
-        client.release();
-    }
-});
-
-// Create product endpoint
-app.post("/products", async (req, res) => {
-    const client = await pool.connect();
-    try {
-        const data = {
-            name: req.body.name,
-            image_url: req.body.image_url,
-            specification: req.body.specification,
-            pros: req.body.pros,
-            cons: req.body.cons,
-            referral_link: req.body.referral_link,
-            video_url: req.body.video_url,
-            tagline: req.body.tagline,
-            created_by: req.body.created_by,
-            created_at: new Date().toISOString(),
-        };
-        const query =
-            "INSERT INTO products (name, image_url, specification, pros, cons, referral_link, video_url, tagline, created_by, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id";
-        const params = [
-            data.name,
-            data.image_url,
-            data.specification,
-            data.pros,
-            data.cons,
-            data.referral_link,
-            data.video_url,
-            data.tagline,
-            data.created_by,
-            data.created_at,
-        ];
-        const result = await client.query(query, params);
-        data.id = result.rows[0].id;
-        res.json({
-            status: "success",
-            data: data,
-            message: "Product created successfully",
-        });
-    } catch (error) {
-        console.error("error", error.message);
     } finally {
         client.release();
     }
